c43cae5e3d8cf92c3f38b86c4cd36cd8d9f762e8
Joe Bitz <joe.bitz@gmail.com>
2020-01-27 22:36:35 -0700

    add sql for sample table and data

--- Data/MusicStoreService.cs
+++ Data/MusicStoreService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,5 +19,13 @@ namespace MyBlazorServerTest.Data
                 }
             });
         }
+
+        // public IAsyncEnumerable<Album> GetAllAlbums()
+        // {
+        //     using (var context = new MusicStoreContext())
+        //     {
+        //         return context.Albums.ToAsyncEnumerable();
+        //     }
+        // }
     }
 }
---
+++ scripts/bootstrap.sql
@@ -0,0 +1,18 @@
+create table 
+    album (
+        id int not null primary key auto_increment,
+        name char(25),
+        artistname char(30),
+        price decimal(2),
+        genre char(30)
+    );
+    
+insert into 
+    album 
+        (name, artistname, price, genre) 
+    values
+        ('One', 'AKON', 15, 'pop'),
+        ('MyLove', 'AKON', 15, 'pop'),
+        ('destiny', 'AKON', 15, 'pop'),
+        ('MySongs', 'John', 15, 'pop');
+
